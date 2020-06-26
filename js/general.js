1a58b0ce1867841ac3ecc983764fb9ab60986e63
Alvaro Gomez <alvarogm4597@gmail.com>
2020-06-26 14:51:32 +0200

    update 26/06

--- js/general.js
+++ js/general.js
@@ -21,12 +21,18 @@ const setDarkTheme = () => {
     document.documentElement.style.setProperty('--background', '#202020');
     document.documentElement.style.setProperty('--color', '#B8ACD1');
     document.querySelector("main").style.backgroundColor = "rgba(32, 32, 32, 0.6)";
+    document.querySelector("aside").style.backgroundColor = "rgba(32, 32, 32, 0.6)";
+    document.querySelector("#dark svg").style.opacity = "1";
+    document.querySelector("#light svg").style.opacity = "0.5";
 }
 
 const setLightTheme = () => {
     document.documentElement.style.setProperty('--background', '#B8ACD1');
     document.documentElement.style.setProperty('--color', '#202020');
     document.querySelector("main").style.backgroundColor = "rgba(184, 172, 209, 0.6)";
+    document.querySelector("aside").style.backgroundColor = "rgba(184, 172, 209, 0.6)";
+    document.querySelector("#dark svg").style.opacity = "0.5";
+    document.querySelector("#light svg").style.opacity = "1";
 }
 
 document.getElementById("burger").addEventListener("click", function(){
